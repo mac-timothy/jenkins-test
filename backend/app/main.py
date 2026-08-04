0a902568d00c7e1eb6c345340e8c12d7e63ea3a4
Mac Timothy Mafabi <mactimothy07@gmail.com>
2026-08-04 13:06:40 +0300

    update FastAPI application startup configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,44 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+
+from app.database.database import engine, Base
+
+from app.models import user
+
+
+from app.routes import auth
+
+
+
+Base.metadata.create_all(
+    bind=engine
+)
+
+
+
+app = FastAPI(
+
+    title="Jenkins Test FastAPI Backend",
+
+    description="Authentication API",
+
+    version="1.0.0"
+
+)
+
+
+
+app.include_router(
+    auth.router
+)
+
+
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the FastAPI application!"}
+
+    return {
+
+        "message": "Welcome to the FastAPI application!"
+
+    }
